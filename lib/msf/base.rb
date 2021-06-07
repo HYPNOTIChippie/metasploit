76c6778f2d7db201a3ac4022dd34b49e783ad8a4
HYPNOTIChippie <84239324+HYPNOTIChippie@users.noreply.github.com>
2021-06-06 19:18:18 -0500

    Initialize land-pr14647

---
+++ lib/msf/base.rb
@@ -0,0 +1,5 @@
+# -*- coding: binary -*-
+
+# Utilities
+require 'msf/util'
+
---
+++ test/tests/testbase.rb
@@ -0,0 +1,8 @@
+#!/usr/bin/env
+
+$:.unshift(File.join(File.expand_path(File.dirname(__FILE__)), '..', '..', 'lib'))
+
+require 'rex'
+require 'msf/ui'
+
+$msf = Msf::Simple::Framework.create
